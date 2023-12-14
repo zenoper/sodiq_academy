0f2596a4dc0745f8fb4600e89a2f2004191c72c6
Parzivald <bez@Muhammadqodirs-Mac.local>
2023-12-14 17:44:29 +0500

    fixing mimees

--- SodiqAcademy/urls.py
+++ SodiqAcademy/urls.py
@@ -1,9 +1,9 @@
+
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('serve/<path:file_path>/', views.serve_file, name='serve_file'),
     path('', views.landing_page, name="landing_page"),
 ]
--- SodiqAcademy/views.py
+++ SodiqAcademy/views.py
@@ -1,23 +1,6 @@
-import mimetypes
+# yourapp/views.py
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from pathlib import Path
-
-def serve_file(request, file_path):
-    # Your other view logic here
-
-    # Get the file extension and determine the content type
-    file_extension = Path(file_path).suffix
-    content_type, encoding = mimetypes.guess_type(file_path)
-    if not content_type:
-        content_type = 'application/octet-stream'  # Default content type for unknown file types
-
-    # Create an HttpResponse object and set the content type
-    response = HttpResponse(content_type=content_type)
-
-    # Render the file
-    return render(request, file_path, context={}, HttpResponse=response)
 
 def landing_page(request):
-    # Your landing page view logic here
-    return render(request, 'SodiqAcademy/index.html', context={})
+    return render(request, 'SodiqAcademy/index.html')
