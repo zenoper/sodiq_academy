fd4ba8ae5b063c3556fea9295e8d6f7876d6a1e8
Parzivald <bez@Muhammadqodirs-Mac.local>
2023-12-15 19:35:10 +0500

    l

--- SodiqAcademy/settings.py
+++ SodiqAcademy/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_SSL_REDIRECT = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
